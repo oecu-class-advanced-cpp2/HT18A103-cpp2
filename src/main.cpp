6a42408d744721e50b5a2d2dfadac311cc8aadec
テツヤ <70543923+wcliper0831@users.noreply.github.com>
2021-11-09 12:47:27 +0900

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
 #define CPP2_PRIME_UPPER_LIMIT 1000000  // 探索する値の上限値
+/**
+*is_prime
+*与えられた正整数xに対して素数かどうか判定する。
+*/
+bool is_prime(unsigned int x) {
+	if (x < 2)return false;
+	else if (x == 2)return false;
+	else if (x % 2 == 0)return false;
+
+	for (int i = 3; i < x; i += 2) {
+		if (x % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
 /* --------------------------------------------------------------- */
 /*
 * nth_prime
@@ -14,16 +30,19 @@
 /* -------------------------------------------------------------- */
 // 与えられた整数a,d,nに対して、この等差数列に含まれるn番目の素数を返す
 int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
-	int num;  // 初期値aを代入する
-	while (num < 10000)
+	int num = a;  // 初期値aを代入する関数num
+	int count = 0;  // 
+	for(int i = 0; i < 100; i++)
 	{
+		std::cout << num << std::endl;
+
 		num += d;
-		std::cout << num << "," << std::endl;
 	}
 };
 
 int main() {
 	std::cout << nth_prime(367, 186, 151) << std::endl;
+	std::cin.get();
 	return;
 
 }
